7ee838ab6a0b47c32e9f3632b028532e37348b70
RUSHI <rushiaher02@gmail.com>
2023-03-16 22:04:31 +0530

    Be a Partner page almost done

---
+++ src/app/components/partnership-card/partnership-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-partnership-card',
+  templateUrl: './partnership-card.component.html',
+  styleUrls: ['./partnership-card.component.css']
+})
+export class PartnershipCardComponent {
+
+}
