5a0fbd44d666c6315b1c8cea5655cf97ede72f76
sha-x-lil <shalil@whizzact.com>
2023-03-19 15:34:05 +0530

    shalil-side

--- src/app/pages/find-rawmatrial/find-rawmatrial.component.ts
+++ src/app/pages/find-rawmatrial/find-rawmatrial.component.ts
@@ -7,6 +7,26 @@ import { Component } from '@angular/core';
 })
 export class FindRawmatrialComponent {
 
-constructor(){window.scrollTo(0,0)}
+  constructor() { window.scrollTo(0, 0) }
+
+
+  elements = [
+    {
+      name: 'metals',
+      icon: '../../../assets/asserts/svgs/metal.svg'
+    },{
+      name: 'chemicals',
+      icon: '../../../assets/asserts/svgs/chemical.svg'
+    },{
+      name: 'polymers',
+      icon: '../../../assets/asserts/svgs/polymers.svg'
+    },{
+      name: 'composites',
+      icon: '../../../assets/asserts/svgs/composites.svg'
+    },{
+      name: 'ceramics',
+      icon: '../../../assets/asserts/svgs/ceramic.svg'
+    },
+  ]
 
 }
