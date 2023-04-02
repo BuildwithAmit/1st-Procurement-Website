ad582c1bbd39173d235ca10d653e14a0870de554
Juned Pothawale <junedwebsole@gmail.com>
2023-04-03 03:15:59 +0530

    Upload Image Api Fetch Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,13 +14,14 @@ import { BeaPartnerComponent } from './pages/bea-partner/bea-partner.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { JoinBetaComponent } from './pages/join-beta/join-beta.component';
-
+import { FormsModule }   from '@angular/forms';
 //third party libraries
 import {SimpleScrollSpyModule} from "angular-simple-scroll-spy";
 import { FAQComponent } from './components/faq/faq.component';
 import { TeamCarouselComponent } from './components/team-carousel/team-carousel.component';
 import { TermsConditionsComponent } from './pages/terms-conditions/terms-conditions.component';
 import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -45,6 +46,8 @@ import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.co
     AppRoutingModule,
     CarouselModule ,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule
 
   ],
   providers: [],
